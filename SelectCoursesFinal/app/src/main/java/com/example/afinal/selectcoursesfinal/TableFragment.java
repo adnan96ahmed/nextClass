dc0c1fb2e6eb2e48bece8f004010b5d0b5f692c4
Mohamed Mohyeldin <33186941+mmohyeld@users.noreply.github.com>
2018-11-29 08:20:45 -0500

    Draw time table

--- SelectCoursesFinal/app/src/main/java/com/example/afinal/selectcoursesfinal/TableFragment.java
+++ SelectCoursesFinal/app/src/main/java/com/example/afinal/selectcoursesfinal/TableFragment.java
@@ -9,12 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TableFragment extends Fragment {
+    private TextView textView;
+    private String jsonString = "courses: { “dept”: MATH, “code”: 4240,  “sections”: [{“ref”:1,“meetings”:[{“days”:10,“type”:“LEC”,“building”:“MCKN”,“room”:“230\",“timeStart”:1000,“timeEnd”:1120}]}] }";
 
     public TableFragment(){
         // Required empty public constructor
@@ -29,7 +36,24 @@ public class TableFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_table, container, false);
+        View view = inflater.inflate(R.layout.fragment_table, container, false);
+        drawSchedule(view);
+        return view;
+    }
+
+    public void drawSchedule(View view){
+        try {
+            JSONObject schedule = new JSONObject(jsonString);
+            JSONArray courses = schedule.getJSONArray("courses");
+            JSONObject course = courses.getJSONObject(0);
+            String dept = course.getString("dept");
+            String code = course.getString("code");
+            String full = dept + code;
+            textView = view.findViewById(R.id.mon1030);
+            textView.setText(full);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
